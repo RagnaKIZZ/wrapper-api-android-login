cdeb3d7d6c7e8847decc380c53a540c9e01343e9
ahmadrusdani80@gmail.com <Ahmedt99>
2021-01-21 20:58:30 +0700

    add logging

--- wrapperApiAndroidLogin/src/main/java/com/amd/flick/wrapperapiandroidlogin/MyLoginWrap.kt
+++ wrapperApiAndroidLogin/src/main/java/com/amd/flick/wrapperapiandroidlogin/MyLoginWrap.kt
@@ -2,13 +2,24 @@ package com.amd.flick.wrapperapiandroidlogin
 
 import android.app.Application
 import com.androidnetworking.AndroidNetworking
+import com.facebook.stetho.Stetho
+import com.facebook.stetho.okhttp3.StethoInterceptor
+import okhttp3.OkHttpClient
+import java.util.concurrent.TimeUnit
 
 class MyLoginWrap : Application() {
 
     override fun onCreate() {
         super.onCreate()
-
-        AndroidNetworking.initialize(this)
+        Stetho.initializeWithDefaults(this)
+        val okHttpClient = OkHttpClient().newBuilder()
+            .addNetworkInterceptor(StethoInterceptor())
+            .connectTimeout(120, TimeUnit.SECONDS)
+            .readTimeout(120, TimeUnit.SECONDS)
+            .writeTimeout(120, TimeUnit.SECONDS)
+            .build()
+        AndroidNetworking.enableLogging()
+        AndroidNetworking.initialize(this, okHttpClient)
 
     }
 
